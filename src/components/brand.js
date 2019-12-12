4ca85ccacb1a473d215807f64e7937d8f1c08b72
github username <github email address>
2019-12-11 16:51:13 -0800

    adding functions in brandSelector.js and brand.js

--- src/components/brand.js
+++ src/components/brand.js
@@ -3,4 +3,16 @@ class Brand {
         this.name = name
     }
 
+
+    static async retrueveAll() {
+        try {
+            const brandObjs = await BrandAdapter.instance.getBrands()
+            return brandObjs.map(obj => new this(obj))
+        }catch(err){
+            alert(`The request failed with ${err}`)
+            return null
+        }
+
+    }
+
 }
--- src/components/brandSelector.js
+++ src/components/brandSelector.js
@@ -2,13 +2,22 @@ class BrandSelector {
     constructor() {
         this.brands = []
         this.bindingsAndEventListeners()
-
+        this.fetchAndLoadBrands()
     }
 
 
     bindingsAndEventListeners() {
         this.container = document.querySelector('#brand-container')
+        this.allBrandsContainer = document.querySelector("#all-brands")
+    }
 
+    async fetchAndLoadBrands() {
+        this.brands = await Brand.retrieveAll()
+        this.render()
+    }
+
+    render() {
+        this.allBrandsContainer.innerHTML = this.brands.map(brand => brand.html).join('')
     }
 
 }
